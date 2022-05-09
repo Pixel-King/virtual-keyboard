5aa835da8e35c09820f113c196f40dbe4404db35
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2022-05-10 01:38:14 +0300

    fix: webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  // mode: 'development"',
   plugins: [
     new HtmlWebpackPlugin({
         title: 'Virtual-keyboard',
